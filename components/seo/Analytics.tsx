7a7737ae2b7460a544cfef074ef845474444ad0a
ankur111kr <ankur111kr@gmail.com>
2026-05-14 20:31:03 +0530

    Add files via upload

---
+++ components/seo/Analytics.tsx
@@ -0,0 +1,50 @@
+import Script from 'next/script'
+
+export function Analytics() {
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID
+
+  return (
+    <>
+      {/* Google Analytics 4 */}
+      {gaId && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga4" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}', { page_path: window.location.pathname });
+            `}
+          </Script>
+        </>
+      )}
+
+      {/* Microsoft Clarity */}
+      {clarityId && (
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${clarityId}");
+          `}
+        </Script>
+      )}
+
+      {/* Google AdSense */}
+      {process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
+        <Script
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      )}
+    </>
+  )
+}
